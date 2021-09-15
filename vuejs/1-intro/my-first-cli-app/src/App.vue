25f6de65998c2a50086e2907f259fb65de2cfbb8
radeax <joflip@gmail.com>
2021-09-15 12:19:22 -0400

    CLI Practice

---
+++ vuejs/1-intro/my-first-cli-app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import LearningList from './components/LearningList.vue'
+</script>
+
+<template>
+  <LearningList name="Sophie" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs/1-intro/my-first-cli-app/src/components/LearningList.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ name }}'s List of Things to Learn</h1>
+
+  <ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Vue</li>
+    <li>Firebase</li>
+  </ul>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>

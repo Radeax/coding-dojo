279ad3721340551d3f5c2a4391d2c2c562eed04e
radeax <joflip@gmail.com>
2021-09-15 11:02:44 -0400

    Practice Directives

---
+++ vuejs/1-intro/practice-directives/index.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+  el: '#root',
+  data: {
+    name: 'Sophie',
+    tech: ['HTML', 'CSS',' Vue', 'Firebase'],
+    financial: ['investing', 'budgeting', 'balance a checkbook']
+  }
+});
